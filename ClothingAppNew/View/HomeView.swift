5e4a07d27bb093c1e6dcad1fab9030783fb6775d
Amasha997 <amashanuwanthi777@gmail.com>
2024-03-21 13:37:49 +0530

    AddProductToHomeScreen

--- ClothingAppNew/View/HomeView.swift
+++ ClothingAppNew/View/HomeView.swift
@@ -14,6 +14,8 @@ struct HomeView: View {
 //    @State var placeholder: String = "placeholder"
 //    @Binding var txt: String
     
+    var didAddCart: ( ()->() )?
+    
     var body: some View {
         ZStack {
             ScrollView {
@@ -50,6 +52,69 @@ struct HomeView: View {
                 .padding(.horizontal, 20)
                 .padding(.vertical, 10)
                 
+                // Exclusive offer products
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: -8) {
+                        ForEach (0...5, id:\.self) {
+                            index in
+                            
+                            VStack {
+                                Image("Jacket")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 130, height: 130)
+                                
+                                Spacer()
+                                
+                                Text("Modern Light Clothes")
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundStyle(.black)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                Text("1pcs, price")
+                                    .font(.system(size: 14, weight: .semibold))
+                                    .foregroundStyle(.gray)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                
+                                HStack {
+                                    Text("$127.90")
+                                        .font(.system(size: 16, weight: .bold))
+                                        .foregroundStyle(.black)
+                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                    
+                                    Spacer()
+                                    
+                                    Button {
+                                        didAddCart?()
+                                    } label: {
+                                        Image(systemName: "plus")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 15, height: 15)
+                                            .accentColor(.white)
+                                    }
+                                    .frame(width: 40, height: 40)
+                                    .background(Color.black)
+                                    .cornerRadius(20)
+                                }
+                                
+                            }
+                            .padding(15)
+                            .frame(width: 200, height: 280)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                            )
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 4)
+                    }
+                }
+                
+                // New Arrival Product
+                
                 HStack {
                     Text("New Arrival")
                         .font(.system(size: 24, weight: .semibold))
@@ -64,6 +129,65 @@ struct HomeView: View {
                 .padding(.horizontal, 20)
                 .padding(.vertical, 10)
                 
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: -8) {
+                        ForEach (0...5, id:\.self) {
+                            index in
+                            
+                            VStack {
+                                Image("Jacket")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 130, height: 130)
+                                
+                                Spacer()
+                                
+                                Text("Modern Light Clothes")
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundStyle(.black)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                Text("1pcs, price")
+                                    .font(.system(size: 14, weight: .semibold))
+                                    .foregroundStyle(.gray)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                
+                                HStack {
+                                    Text("$127.90")
+                                        .font(.system(size: 16, weight: .bold))
+                                        .foregroundStyle(.black)
+                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                    
+                                    Spacer()
+                                    
+                                    Button {
+                                        didAddCart?()
+                                    } label: {
+                                        Image(systemName: "plus")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 15, height: 15)
+                                            .accentColor(.white)
+                                    }
+                                    .frame(width: 40, height: 40)
+                                    .background(Color.black)
+                                    .cornerRadius(20)
+                                }
+                                
+                            }
+                            .padding(15)
+                            .frame(width: 200, height: 280)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                            )
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 4)
+                    }
+                }
+                
                 HStack {
                     Text("Best Selling")
                         .font(.system(size: 24, weight: .semibold))
@@ -77,6 +201,65 @@ struct HomeView: View {
                 }
                 .padding(.horizontal, 20)
                 .padding(.vertical, 10)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: -8) {
+                        ForEach (0...5, id:\.self) {
+                            index in
+                            
+                            VStack {
+                                Image("Jacket")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 130, height: 130)
+                                
+                                Spacer()
+                                
+                                Text("Modern Light Clothes")
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundStyle(.black)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                Text("1pcs, price")
+                                    .font(.system(size: 14, weight: .semibold))
+                                    .foregroundStyle(.gray)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                
+                                HStack {
+                                    Text("$127.90")
+                                        .font(.system(size: 16, weight: .bold))
+                                        .foregroundStyle(.black)
+                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                    
+                                    Spacer()
+                                    
+                                    Button {
+                                        didAddCart?()
+                                    } label: {
+                                        Image(systemName: "plus")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 15, height: 15)
+                                            .accentColor(.white)
+                                    }
+                                    .frame(width: 40, height: 40)
+                                    .background(Color.black)
+                                    .cornerRadius(20)
+                                }
+                                
+                            }
+                            .padding(15)
+                            .frame(width: 200, height: 280)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                            )
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 4)
+                    }
+                }
             }
         }
     }
